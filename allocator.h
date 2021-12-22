8bceef0485600872be2c742c3c4904ce78bee73c
Bob Aaldering <74597240+BobAaldering@users.noreply.github.com>
2021-12-22 11:32:44 +0100

    Added more functions to 'allocator.h'.

--- allocator.h
+++ allocator.h
@@ -9,14 +9,43 @@ namespace memory_map {
     template <typename T>
     class allocator {
     public:
-        typedef T value_type;
+        [[maybe_unused]] typedef T value_type;
+        [[maybe_unused]] typedef T* pointer;
+        [[maybe_unused]] typedef const T* const_pointer;
+        [[maybe_unused]] typedef T& reference;
+        [[maybe_unused]] typedef const T& const_reference;
+        [[maybe_unused]] typedef std::size_t size_type;
+        [[maybe_unused]] typedef std::ptrdiff_t difference_type;
 
-        allocator() = default;
+        // This is for rebinding to the type 'U'.
+        template<class U>
+        struct [[maybe_unused]] rebind {
+            [[maybe_unused]] typedef allocator<U> other_allocator;
+        };
+
+        // Return the address of your 'value', you can modify its content.
+        [[maybe_unused]] pointer address(reference value) const {
+            return &value;
+        }
+
+        // Return the address of your 'value', you cannot modify its content, because its constant.
+        [[maybe_unused]] const_pointer address(const_reference value) const {
+            return &value;
+        }
+
+        // Use default constructors, because 'allocator' will have no state.
+        allocator() noexcept = default;
+        allocator(const allocator&) noexcept = default;
+        ~allocator() noexcept = default;
+
+        [[nodiscard]] size_type max_size() const noexcept {
+            return std::numeric_limits<size_type>::max() / sizeof(T);
+        }
 
         template<class U> constexpr explicit allocator (const allocator<U>&) noexcept {}
 
-        [[nodiscard]] T* allocate(std::size_t number_of_elements) {
-            if (number_of_elements > (std::numeric_limits<std::size_t>::max() / sizeof(T)))
+        [[maybe_unused]] [[nodiscard]] T* allocate(std::size_t number_of_elements) {
+            if (number_of_elements > max_size())
                 throw std::bad_array_new_length();
 
             if (auto ptr = static_cast<T*>(mmap(nullptr, number_of_elements, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0))) {
@@ -27,7 +56,16 @@ namespace memory_map {
             throw std::bad_alloc();
         }
 
-        void deallocate(T* ptr, std::size_t number_of_elements) noexcept {
+        // Constructs an object of the type in you previously-allocated storage at the address pointed to 'ptr', using 'value' as the constructor arguments.
+        [[maybe_unused]] void construct(pointer ptr, const_reference value) {
+            new ((void*)ptr) T(value);
+        }
+
+        [[maybe_unused]] void destroy(pointer ptr) {
+            ptr->~T();
+        }
+
+        [[maybe_unused]] void deallocate(T* ptr, std::size_t number_of_elements) noexcept {
             used_memory_message(ptr, number_of_elements, false);
             if (munmap(ptr, number_of_elements))
                 std::cerr << "Deallocating memory failed!" << std::endl;
--- main.cpp
+++ main.cpp
@@ -6,8 +6,24 @@
 // Link: https://en.cppreference.com/w/cpp/named_req/Allocator
 
 int main() {
-    std::vector<int, memory_map::allocator<int>> vector(8);
+    std::vector<int, memory_map::allocator<int>> vector{};
     vector.push_back(8);
+    vector.push_back(9);
+    vector.push_back(10);
+    vector.push_back(11);
+    vector.push_back(12);
+
+    memory_map::allocator<double> alloc{};
+
+    auto* mem = alloc.allocate(45);
+
+    for (int i = 0; i < 45; i++)
+        mem[i] = i;
+
+    for (int i = 0; i < 45; i++)
+        std::cout << "Value: " << mem[i] << std::endl;
+
+    alloc.deallocate(mem, 45);
 
     return 0;
 }
